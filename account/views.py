c2148ba6c37e9a7398126de4ddab3d2946021829
= <=>
2023-11-12 16:13:29 +0300

    logout

--- account/views.py
+++ account/views.py
@@ -3,12 +3,14 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from .serializers import UserSerializer, UserLoginSerializer
+# from django.contrib.auth import authenticate, login
 from django.contrib.auth import authenticate, login
+from .models import CustomUser
+from django.views.decorators.csrf import csrf_exempt
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def register_user(request):
-    print("o")
     if request.method == 'POST':
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
@@ -24,8 +26,9 @@ def user_login(request):
     if serializer.is_valid():
         username = serializer.validated_data['username']
         password = serializer.validated_data['password']
-        user = authenticate(request, username=username, password=password)
-
+        # user = authenticate(request, username=username, dpassword=password)
+        user = CustomUser.objects.get(username=username, password=password)
+        print(user)
         if user is not None :
             login(request, user)
             return Response({'message': 'Login successful'}, status=status.HTTP_200_OK)
@@ -33,3 +36,4 @@ def user_login(request):
             return Response({'message': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
     else:
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
