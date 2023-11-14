f604335cf693c5e2dee519e0c6e5440dfb761d39
= <=>
2023-11-14 11:59:39 +0300

    custom permission

--- account/models.py
+++ account/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class CustomUser(AbstractUser):
+    ROLES = [
+        ('admin', 'Admin'),
+        ('tech', 'Tech'),
+        ('customer', 'Customer'),
+    ]
     # adding the custom field here
+    role = models.CharField(max_length=20, choices=ROLES, default='customer')
     profile_pic = models.CharField(max_length=100, null=True)
     pass
