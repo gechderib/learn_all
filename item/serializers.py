23bc071f9d534b8a000d190fe692d56bff985085
= <=>
2023-11-26 23:56:20 +0300

    item model

--- item/serializers.py
+++ item/serializers.py
@@ -1,7 +1,11 @@
 from rest_framework import serializers
 from .models import Item
+from category.serializers import SubCategorySerializer, CategorySerializer
+from account.serializers import UserSerializer
 class ItemSerializer:
-
+    subcategories = SubCategorySerializer(many=False, read_only=True)
+    category = CategorySerializer()
+    postedBy = UserSerializer(many=False,)
     class Meta:
         model = Item
         fields = ('id', 'name','description','category','subcategory','postedBy','images')
