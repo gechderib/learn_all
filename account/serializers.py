e371e6598cd31d87e955fef515e5ccb10abc33b2
gechderibm <gechderib@gmail.com>
2023-12-03 16:51:54 +0300

    user data'

--- account/serializers.py
+++ account/serializers.py
@@ -4,12 +4,12 @@ import phonenumbers
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
-    # roles = serializers.ListSerializer(child=serializers.CharField(max_length=20), default=["renter"])
+    roles = serializers.ListSerializer(child=serializers.CharField(max_length=20), default=["renter"])
     profile_pic = serializers.ImageField(max_length=None, allow_empty_file=True, use_url=True)
 
     class Meta:
         model = CustomUser
-        fields = ('id', 'username', 'password', 'email','phone_number','first_name', 'last_name','profile_pic')
+        fields = ('id', 'username', 'password', 'email','phone_number','first_name', 'last_name','profile_pic','roles')
     
 class UserLoginSerializer(serializers.Serializer):
     password = serializers.CharField(write_only=True)
