3043aa7d7b46f107dd813a23d09a1d558ce3fd11
gechderibm <gechderib@gmail.com>
2023-12-06 15:36:25 +0300

    model fix

--- item/models.py
+++ item/models.py
@@ -29,7 +29,7 @@ class Item(models.Model):
     subcategory = models.ForeignKey(SubCategory, on_delete=models.CASCADE, related_name="subcategory")
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='category')
     postedBy = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=False, blank=False, related_name='postedBy')
-    images = models.TextField(blank=True)  # Store image URLs as a comma-separated string
+    images = models.TextField(blank=True, null=True)  # Store image URLs as a comma-separated string
 
     # images = ArrayField(models.ImageField(upload_to='item_images/'), blank=True, null=True)
     
