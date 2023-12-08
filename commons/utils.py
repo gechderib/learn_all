3b89a6e8843b29b8cb55977050e7d02224692a9e
gechderibm <gechderib@gmail.com>
2023-12-08 11:43:41 +0300

    bug fix for image upload

--- commons/utils.py
+++ commons/utils.py
@@ -9,10 +9,7 @@ def get_public_id_from_url(image_url):
 
     return public_id
 
-def delete_cloudinary_image(image_url):
-
-    # Get the public ID from the Cloudinary API
-    public_id = get_public_id_from_url(image_url)
+def delete_cloudinary_image(public_id):
     if public_id:
         # Delete the image by its public ID
         delete_resources([public_id])
--- item/views.py
+++ item/views.py
@@ -8,7 +8,7 @@ from rest_framework.parsers import MultiPartParser, FormParser
 from cloudinary import uploader
 from commons.middlewares import isImageExist
 from django.shortcuts import get_object_or_404
-from commons.utils import get_public_id_from_url, delete_cloudinary_image
+from commons.utils import  delete_cloudinary_image
 # Create your views here.
 @api_view(['GET'])
 def get_all_items(request):
@@ -48,8 +48,8 @@ def add_item(request):
 
             image_urls = []
             item_folder = "item_images"
-            for i in range(len(images)):
-                result = uploader.upload(images[i], folder=item_folder)
+            for image in images:
+                result = uploader.upload(image, folder=item_folder)
                 image_urls.append(result['secure_url'])
             instance = Item.objects.create(name=name, category=category,subcategory=subcategory,postedBy=postedBy,description=description,status=status,rent_price=rent_price,available_for_sell=available_for_sell, selling_price=selling_price,image_urls=image_urls)
             serializer = ItemCreateSerializer(instance)
@@ -67,12 +67,16 @@ def update_item(request, pk):
             # Check if images are being updated
             if 'images' in request.FILES:
                 # remove the previous image from cloudinary
-                for img_url in instance.image_urls:
-                    public_id_from_url = get_public_id_from_url(img_url)
-                    delete_cloudinary_image(public_id_from_url)
-                    
-                # add the new image to cloudinary
                 item_folder = "item_images"
+                for img_url in instance.image_urls:
+                    parts = img_url.split('/')
+                    public_id = item_folder +"/"+ parts[-1].split('.')[0]
+                    print(public_id)
+                    aaa = delete_cloudinary_image(public_id)
+                    print("aaa = ", aaa)
+
+                # add the new image to cloudinary
+                
                 new_image_urls = []
                 for new_image in request.FILES.getlist('images'):
                     result = uploader.upload(new_image, folder=item_folder)
