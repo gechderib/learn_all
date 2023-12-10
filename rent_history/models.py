fc62c5d02ae7dfbe7ec87ce57d0a66b44d065cc7
gechderibm <gechderib@gmail.com>
2023-12-10 14:34:16 +0300

    model for history

--- rent_history/models.py
+++ rent_history/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class SubCategory(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True, unique=False)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,related_name='subcategories')
+
+    def __str__(self):
+        return f"{self.category.name} - {self.name}"
