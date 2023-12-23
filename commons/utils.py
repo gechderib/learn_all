4d753367b22e3520c749c21bd0d7affe26cfa9c5
gechderib <gechderib@gmail.com>
2023-12-23 17:10:59 +0300

    public id

--- commons/utils.py
+++ commons/utils.py
@@ -11,5 +11,6 @@ def get_public_id_from_url(image_url):
 
 def delete_cloudinary_image(public_id):
     if public_id:
-        # Delete the image by its public ID
+        # Delete the image by its public ID 
+        # delete the resour by id
         delete_resources([public_id])
